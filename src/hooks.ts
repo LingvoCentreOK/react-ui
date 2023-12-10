6ff3aaf9d509faccf0cf6f8c578ae45dca955a4b
Pavlo Lushnykov <lushnikov.vasa@gmail.com>
2023-12-10 10:08:06 +0200

    delete some shit

--- src/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
-import type { AppDispatchType, RootStateType } from "./store/store";
-
-export const useAppDispatch: () => AppDispatchType = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector;
